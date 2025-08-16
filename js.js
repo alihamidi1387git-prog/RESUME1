a7cd297874420f15e7d89d73cc012890c1c1f3d7
alihamidi1387git-prog@gmail.com <ali.hamidi1401400@gmail.com>
2025-08-16 11:50:37 +0330

    Revert "rewrite last commit"
    This reverts commit 5478cc87c0bb890b948658850e3448d4d33abbf4.

--- js.js
+++ js.js
@@ -1,37 +1,12 @@
-function wakeUp()
-{
-    return new Promise((resolve , reject) =>
-    {
-        setTimeout(() => {
-            console.log('wake up');
-            
-            resolve({id:1 , name:'jam & bread' , drink:"milk"})
-        }, 2000);
-    })
-}
-function eatBreakFast(food)
-{
-    return new Promise((resolve , reject) =>
-    {
-        setTimeout(() => {
-            console.log(`wow my fod is ${food.name} and with ${food.drink} , thanks for god💕`);
-            resolve('24 min & 39 sec')
-        }, 2000);
-    })
-}
-function goWork(time)
-{
-    return new Promise((resolve , reject) =>
-    {
-        setTimeout(() => {
-          console.log(`oh no ! my eating long ${time} , i must go to work`);
-          resolve("finished !")
-        }, 2000);
-    })
-}
-wakeUp()
-.then(eatBreakFast
+// mutable & immutable 
+// mutable => array , object , date 
+// immutable => string , number , age
 
-)
-.then(goWork)
-.then(response3 => console.log(response3))
+
+
+let arr = [1,2,3,4,5];
+
+
+[...arr].push("mio")
+
+
